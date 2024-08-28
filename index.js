777d74f4c5f40ea809d91eb613c04461a8d8bb74
euno4h <163136318+euno4h@users.noreply.github.com>
2024-08-28 07:56:06 -0300

    .

---
+++ index.js
@@ -0,0 +1,32 @@
+var nome = "John";
+
+let idade = 25;
+
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b;
+console.log(soma);
+
+let a = 5;
+let b = 10;
+
+function multiplica(x,y) {
+    return x * y
+};
+
+let produto = multiplica (a,b);
+console.log(produto);
+
+let frutas = ['apple','banana','cherry'];
+console.log(frutas[1]);
+
+let pessoa = {
+    primeironome: 'john',
+    sobrenome: 'delon',
+    age: 25,
+};
+console.log(pessoa.primeironome);
+
+
